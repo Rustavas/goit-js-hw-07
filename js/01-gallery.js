940186089eb79e50cbd98f4127425368051623a8
RuslanIvasiunko <115837907+Rustavas@users.noreply.github.com>
2023-03-28 19:35:56 +0300

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+} ;
+
+const listItems = galleryItems.map(({preview, original, description}) => {
+return `<li class='gallery__item'>
+<a class='gallery__link' href='${original}'
+<img class="gallery__image"
+src="${preview}"
+data-source='${original}'
+alt='${description}'
+/></a></li>`;
+}).join('');
+
+refs.gallery.insertAdjacentHTML('beforeend', listItems);
+
+console.log(refs.gallery);
+
+// console.log(galleryItems);
+
+refs.gallery.addEventListener('click', onOpenModal)
+
+function onOpenModal(event){
+  event.preventDefault();
+  const currentItem = event.target;
+console.log(currentItem.nodeName);
+  if(currentItem.nodeName !== 'IMG'){
+    return;
+  }
+   let instance = basicLightbox.create(`
+      <img width="1400" height="900" src="${currentItem.dataset.source}">
+    `);
+    instance.show()
+    console.dir(currentItem.nodeName);
+}
+
